df980f7c87a264a0adf17cbe495c35a296e784ed
Alex <67165413+Alex-Tairov@users.noreply.github.com>
2024-12-13 16:33:46 +0300

    Add files via upload

---
+++ Task1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Длина массива
+            var arrayLenght = Convert.ToInt32(args[0]);
+            
+            //Длина интервала
+            var interval = Convert.ToInt32(args[1]);
+          
+            //Создание и заполнение массива
+            var arrayNumbers = new int[arrayLenght];
+
+            for (int i = 0; i < arrayLenght; i++)
+            {
+                arrayNumbers[i] = i + 1;
+            }
+
+            //Список хранящий числа,полученные при обходе массива
+            var temporaryResultList = new List<int>();
+
+            //Индекс числа ,полученный при обходе
+            int currentElementIndex = 0;
+
+            //Обход массива
+            do
+            {
+                temporaryResultList.Add(arrayNumbers[currentElementIndex]);
+                currentElementIndex = (currentElementIndex + interval - 1) % arrayLenght;
+            }
+            while (currentElementIndex != 0);
+
+            //Преобразование списка в массив
+            var resultArray = temporaryResultList.ToArray();
+
+            //Вывод массива
+            for (int i = 0; i < resultArray.Length; i++)
+            {
+                Console.Write(resultArray[i]);
+            }
+
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+
+namespace Task2
+{
+    internal class Program
+    {
+        private static void CalculationResult(double hypotenuse,double radiusCircle)
+        {
+            if (hypotenuse == radiusCircle)
+            {
+                Console.WriteLine("0");
+
+            }
+            else if (hypotenuse < radiusCircle)
+            {
+                Console.WriteLine("1");
+            }
+            else if (hypotenuse > radiusCircle)
+            {
+                Console.WriteLine("2");
+            }
+        }
+        static void Main(string[] args)
+        {
+           
+            //Путь до файла,хранящий параметы окружности
+             var cicleFilePath = args[0];
+
+            //Путь до файла,хранящий список точек
+            var pointFilePath = args[1];
+            
+            //Считываем данные из файла с оружностями
+            var dataCircleFile = File.ReadAllLines(@cicleFilePath);
+
+            //Вычисляем координаты окружности и ее радиус
+            var circleCoordinates = dataCircleFile[0].Split();
+            var xCoordinateCenterCircle = Convert.ToDouble(circleCoordinates[0]);
+            var yCoordinateCenterCircle = Convert.ToDouble(circleCoordinates[1]);
+            var radiusCircle = Convert.ToDouble(dataCircleFile[1]);
+
+            //Считываем данные из файла с точками
+            var dataPointFile = File.ReadAllLines(@pointFilePath);
+            for (int i = 0; i < dataPointFile.Length; i++)
+            {
+                //Вычисляем координаты точек и гипотенузу
+                var pointCoordinates = dataPointFile[i].Split();
+                var xPointCoordinate = Convert.ToDouble(pointCoordinates[0]);
+                var yPointCoordinate = Convert.ToDouble(pointCoordinates[1]);
+                var hypotenuse = Math.Sqrt(Math.Pow((xPointCoordinate - xCoordinateCenterCircle), 2)
+                    + Math.Pow((yPointCoordinate - yCoordinateCenterCircle), 2));
+
+                //Выводим результат
+                CalculationResult(hypotenuse, radiusCircle);
+            }
+
+
+
+        }
+    }
+}
